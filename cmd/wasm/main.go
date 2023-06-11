8c9b4ee4ff5a3aaa4947968531317f51efb2314f
akosgarai <szlem01@gmail.com>
2023-06-11 16:02:09 +0200

    use the application in wasm command

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -5,46 +5,13 @@ package main
 
 import (
 	"fmt"
-	"syscall/js"
 
-	"github.com/akosgarai/wasm-example/pkg/formatter"
+	"github.com/akosgarai/wasm-example/pkg/application/client"
+	"github.com/akosgarai/wasm-example/pkg/page/formatter"
 )
-
-func jsonWrapper() js.Func {
-	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if len(args) != 1 {
-			return map[string]interface{}{
-				"error": "Invalid no of arguments passed",
-			}
-		}
-		jsDoc := js.Global().Get("document")
-		if !jsDoc.Truthy() {
-			return map[string]interface{}{
-				"error": "Unable to get document object",
-			}
-		}
-		jsonOuputTextArea := jsDoc.Call("getElementById", "jsonoutput")
-		if !jsonOuputTextArea.Truthy() {
-			return map[string]interface{}{
-				"error": "Unable to get output text area",
-			}
-		}
-		inputJSON := args[0].String()
-		fmt.Printf("input %s\n", inputJSON)
-		pretty, err := formatter.PrettyJSON(inputJSON)
-		if err != nil {
-			return map[string]interface{}{
-				"error": fmt.Sprintf("unable to parse JSON. Error %s occurred\n", err),
-			}
-		}
-		jsonOuputTextArea.Set("value", pretty)
-		return nil
-	})
-	return jsonFunc
-}
 
 func main() {
 	fmt.Println("WASM Go Initialized")
-	js.Global().Set("formatJSON", jsonWrapper())
-	<-make(chan bool)
+	clientApp := client.New(formatter.New("JSON Formatter"))
+	clientApp.Run()
 }
