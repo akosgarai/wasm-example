4968b02f864bd5bb212ab8505c53e87ef372813e
akosgarai <szlem01@gmail.com>
2023-09-18 13:55:54 +0200

    Application model implementation and host extension

--- pkg/server/models/host.go
+++ pkg/server/models/host.go
@@ -5,9 +5,12 @@ import "gorm.io/gorm"
 // Host is the model of the host table.
 type Host struct {
 	gorm.Model
-	ID            uint        `json:"id" gorm:"primaryKey"`
-	Name          string      `json:"name"`                                                     // host name
-	IP            string      `json:"ip" gorm:"uniqueIndex:host_environment_index;size:256"`    // host ip or domain
-	EnvironmentID uint        `json:"environment_id" gorm:"uniqueIndex:host_environment_index"` // environment id
-	Environment   Environment `gorm:"foreignKey:EnvironmentID"`
+	ID            uint         `json:"id" gorm:"primaryKey"`
+	Name          string       `json:"name"`                                                     // host name
+	IP            string       `json:"ip" gorm:"uniqueIndex:host_environment_index;size:256"`    // host ip or domain
+	EnvironmentID uint         `json:"environment_id" gorm:"uniqueIndex:host_environment_index"` // environment id
+	Environment   *Environment `gorm:"foreignKey:EnvironmentID"`
+	SSHUser       string       `json:"ssh_user"` // ssh user
+	SSHPort       int          `json:"ssh_port"` // ssh port
+	SSHKey        string       `json:"ssh_key"`  // ssh key
 }
